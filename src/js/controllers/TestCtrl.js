52c4f7ec84e03474584c737da326f2a0f30fe3d5
Francisco Manzano <keoh77@gmail.com>
2015-10-19 02:51:13 +0200

    Minor fixes

--- src/js/controllers/TestCtrl.js
+++ src/js/controllers/TestCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var TestCtrl = function($scope) {
-  $scope.test = 'Esta en una prueba de controlador en archivo separado y actualizable';
+  $scope.test = 'Text controller works fine!';
 };
 
 module.exports = TestCtrl;
